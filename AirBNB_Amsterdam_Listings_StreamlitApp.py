614817c7ceb55e9955a2c0fbaf92942c1c1dd352
Panagiotis Papastathis <panos.pap10@hotmail.com>
2023-03-17 19:57:50 +0200

    More Consice Title

--- AirBNB_Amsterdam_Listings_StreamlitApp.py
+++ AirBNB_Amsterdam_Listings_StreamlitApp.py
@@ -26,7 +26,7 @@ dataframe = dataframe[dataframe["Price"] <= 100]
 # Display as integer
 dataframe["Airbnb Listing ID"] = dataframe["Airbnb Listing ID"].astype(int)
 # Round of values
-dataframe["Price"] = "€" + dataframe["Price"].round(2).astype(str) 
+dataframe["Price"] = "€" + dataframe["Price"].round(2).astype(str)
 # Rename the number to a string
 dataframe["Location"] = dataframe["Location"].replace(
     {1.0: "To visit", 0.0: "Airbnb listing"}
